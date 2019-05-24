37e501a55e3ee01c162ef5b3977947051185f366
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-05-24 10:25:28 -0500

    have a solution -- fixed

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,9 +2,17 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	let index = Math.floor(array.length/2)
-
-	
+	if (array.length === 0){
+		return false
+	}
+	let index = Math.floor(array.length / 2)
+	if (array[index] === target) {
+		return true
+	} else if (array[index] < target) {
+		return binarySearch(array.slice(0, index))
+	} else {
+		return binarySearch(array.slice(index + 1))
+	}	
 };
 
 /*
